6d1650ae3dd72d88c1dff54eff19f0394ee2dfc3
SergeyBasharkin <rock-n-rolllll@bk.ru>
2016-05-08 21:21:32 +0300

    try fix mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
-      domain: "heroku.com",
       authentication: "plain",
       enable_starttls_auto: true,
       user_name: "sergeybasharkin@gmail.com",
