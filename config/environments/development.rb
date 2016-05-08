9772bb793f95ba39f217c2ba8f7c971b3aed2058
SergeyBasharkin <rock-n-rolllll@bk.ru>
2016-05-08 23:56:06 +0300

    try fix mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,9 +50,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
-      domain: "myprojectdomain.com",
-      user_name: "xxxx@gmail.com",
-      password: "xxxx",
+      domain: "gmail.com",
+      user_name: "sergeybasharkin@gmail.com",
+      password: "ariarrn3336754",
       authentication: 'plain',
       enable_starttls_auto: true
   }
